c2fae37f891ccdaafb55525b3afeed6245cafdb5
jaewonYoon <21300492@handong.edu>
2019-02-18 01:56:04 +0900

    add apply image change throuh option selection..

--- js/sketch.js
+++ js/sketch.js
@@ -5,41 +5,49 @@ let style1;
 let style2;
 
 function setup() {
-  noCanvas();
-  // Status Msg
-  statusMsg = select('#statusMsg');
+    noCanvas();
+    // Status Msg
+    statusMsg = select('#statusMsg');
 
-  // Get the input image
-  inputImg = select('#inputImg');
+    // Get the input image
+    inputImg = select('#inputImg');
 
-  // Transfer Button
-  transferBtn = select('#transferBtn')
-  transferBtn.mousePressed(transferImages);
+    // Transfer Button
+    transferBtn = select('#transferBtn')
+    transferBtn.mousePressed(transferImages);
 
-  // Create two Style methods with different pre-trained models
-  style1 = ml5.styleTransfer('../ckpts/wave', modelLoaded);
-  style2 = ml5.styleTransfer('../ckpts/udnie', modelLoaded);
+    // Create two Style methods with different pre-trained models
+    style1 = ml5.styleTransfer('../ckpts/wave', modelLoaded);
+    style2 = ml5.styleTransfer('../ckpts/udnie', modelLoaded);
 }
 
 // A function to be called when the models have loaded
 function modelLoaded() {
-  // Check if both models are loaded
-  if(style1.ready && style2.ready){
-    statusMsg.html('Ready!')
-  }
+    // Check if both models are loaded
+    if (style1.ready && style2.ready) {
+        statusMsg.html('Ready!')
+    }
 }
 
 // Apply the transfer to both images!
 function transferImages() {
-  statusMsg.html('Applying Style Transfer...!');
-  
-  style1.transfer(inputImg, function(err, result) {
-    createImg(result.src).parent('styleA');
-  });
+    statusMsg.html('Applying Style Transfer...!');
 
-  style2.transfer(inputImg, function(err, result) {
-    createImg(result.src).parent('styleB');
-  });
+    style1.transfer(inputImg, function (err, result) {
+        createImg(result.src).parent('styleA');
+    });
 
-  statusMsg.html('Done!');
+    style2.transfer(inputImg, function (err, result) {
+        createImg(result.src).parent('styleB');
+    });
+
+    statusMsg.html('Done!');
 }
+
+//styletransfer image select
+
+$('.myitem').each(function () {
+    if ($(this).hasClass('selected')) {
+        $('#myImage').attr('src') = 'img/styles' + $(this).attr('id')[1] + '.jpg';
+    }
+}); 
