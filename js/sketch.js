54d33fc1c44faba562a889573a9928dec1564f25
jaewonYoon <21300492@handong.edu>
2019-03-08 11:50:58 +0900

    image resize // css file edited

--- js/sketch.js
+++ js/sketch.js
@@ -1,54 +1,53 @@
-// Copyright (c) 2018 ml5
-//
-// This software is released under the MIT License.
-// https://opensource.org/licenses/MIT
-
-/* ===
-ml5 Example
-Style Transfer Image Example using p5.js
-This uses a pre-trained model of The Great Wave off Kanagawa and Udnie (Young American Girl, The Dance)
-=== */
-
 var image_src;
 var model_src;
-var transferStyle; 
-//image change using select tag 
-$("#style-select").change(function () {
-    cur_value = $('option:selected', this).text();
-    console.log(cur_value);
-    image_src = "img/styles/" + cur_value + ".jpg";
-    model_src = "ckpts/"+cur_value;
-    console.log(image_src);
-    console.log(model_src);
-    $("#style-img").attr("src", image_src);
-    transferStyle = ml5.styleTransfer(model_src,modelLoaded);
+var transferStyle;
+let inputImg; 
+//image change using select tag
+function select_style(selected) {
+  cur_value = selected.text();
+  console.log(cur_value);
+  image_src = "img/styles/" + cur_value + ".jpg";
+  model_src = "ckpts/"+cur_value;
+  console.log(image_src);
+  console.log(model_src);
+  $("#style-img").attr("src", image_src);
+  // Create two Style methods with different pre-trained models
+  transferStyle = ml5.styleTransfer(model_src,modelLoaded);
+}
+function select_content(selected) {
+  cur_value = selected.text();
+  console.log(cur_value);
+  $("#content-img").attr("src", "img/" + cur_value + ".jpg");
+  inputImg = select('#content-img');
+}
+$("#style-select").change(function() {
+  select_style($('option:selected', this));
+});
+$("#content-select").change(function() {
+  select_content($('option:selected', this));
 });
 //
 
-let inputImg;
-let statusMsg;
+// let statusMsg;
 let transferBtn;
-let style1;
-let style2;
+// let style1;
+// let style2;
 
 function setup() {
   //let myCanvas = createCanvas(600,400);
   noCanvas();
   // Get the input image
-  inputImg = select('#content-img');
-
+  select_style($("#style-select option:selected"));
+  select_content($("#content-select option:selected"));
   // Transfer Button
   transferBtn = select('.transferBtn')
   transferBtn.mousePressed(transferImages);
-
-  // Create two Style methods with different pre-trained models
-  style1 = ml5.styleTransfer(model_src, modelLoaded);
 }
 
 // A function to be called when the models have loaded
 function modelLoaded() {
   // Check if both models are loaded
-  if(style1.ready){
+  if(transferStyle.ready){
     console.log('Ready!')
   }
 }
@@ -57,8 +56,8 @@ function modelLoaded() {
 function transferImages() {
   console.log('Applying Style Transfer...!');
 
-  style1.transfer(inputImg, function(err, result) {
-    createImg(result.src).parent('style-img');
+  transferStyle.transfer(inputImg, function(err, result) {
+    $(".outputImg").attr("src", result.src);
   });
 
 }
