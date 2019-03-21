fc34e359f698c5c1dfbbd0853e5d23e5bd94ef81
jaewonYoon <21300492@handong.edu>
2019-03-21 15:38:48 +0900

    add ejs -1 still refactoring continuing

--- js/app.js
+++ js/app.js
@@ -1,31 +1,49 @@
 //jshint enversion: 6
 
+//require module 
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const path = require("path");
+const ejs = require("ejs");
+const load = require("lodash");
 const app = express();
+const url = require('url');    
+
+//use module , set ejs module
 app.use(express.static(path.join(__dirname,'../public')));
 app.use(express.static(path.join(__dirname,'../')));
 app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+
+///variables///
+let posts =[]; 
+const homeStartingContent ="It's your own board. feel free to ask any questions in your board and we wiil reply you ASAP";
 ///routes/// 
 
-app.get("/", function(req,res){
-    res.sendFile(path.join(__dirname,"../index.html"));
+app.get("/",function(req,res){
+   console.log("home directory is underway");
+   res.render("home"); 
 });
 app.get("/pricing", function(req,res){
     res.sendFile(path.join(__dirname,"../pricing.html"));
 });
+ 
 app.get("/signup", function(req,res){
-    res.sendFile(path.join(__dirname,"../signup.html"));
+    if(req.url.query){ 
+      console.log(req.url.query.email);
+      res.render("signup",{email_req: req.url.query.email});
+    }
+    else res.render("signup",{email_req: ''}); //is null 
+//    res.sendFile(path.join(__dirname,"../signup.html"));
 });
 app.post("/signup",function(req,res){
-    var firstName =req.body.fName; 
-    var lastName = req.body.lName;
-    var email = req.body.email;
-    var image = req.body.image;
+    let firstName =req.body.fName; 
+    let lastName = req.body.lName;
+    let email = req.body.email;
+    let image = req.body.image;
     
-    var data = {
+    let data = {
         members: [
             {
                 email_address: email,
@@ -38,8 +56,8 @@ app.post("/signup",function(req,res){
             }
         ]
     };
-    var jsonData = JSON.stringify(data);
-    var options={
+    let jsonData = JSON.stringify(data);
+    let options={
         url:"https://us20.api.mailchimp.com/3.0/lists/eedf70aa88",
         method: "POST",
         headers: {
@@ -52,7 +70,8 @@ app.post("/signup",function(req,res){
             res.sendFile(path.join(__dirname,"../failure.html"));
         } else {
             if(response.statusCode===200){
-                res.sendFile(path.join(__dirname,"../success.html"));
+//                res.sendFile(path.join(__dirname,"../success.html"));
+                res.render("success");
             }else {
                 res.sendFile(path.join(__dirname,"../failure.html"));
             }
@@ -63,16 +82,51 @@ app.post("/signup",function(req,res){
 app.post("/failure", function(req,res){
     res.redirect("/");
 });
+////////routes for about.ejs
 
+app.get("/about",function(req,res){
+   console.log("test for print");
+   res.render("about",{
+       context:homeStartingContent,
+       posts:posts,
+   });
+    
+});
+app.get("/posts/:postName",function(req,res){
+   const requestedTitle=req.params.postName
+   console.log(req.params.postName); 
+   posts.forEach(function(element){
+       if(convert(element.title)===convert(requestedTitle))
+           res.render("post",{title:element.title, body: element.content});
+       else console.log("No match!");
+   });
+});
+/////////routes for compose.ejs
+
+app.get("/compose",function(req,res){
+    res.render("compose");
+})
+app.post("/compose",function(req,res){
+   const post ={
+   title : req.body.postTitle,
+   content : req.body.postBody
+   };
+   posts.push(post);
+   res.redirect("/");
+});
+//////////routes for masthead
+app.post("/masthead",function(req,res){
+    let email = req.body.email;
+    console.log(email);
+    res.redirect(url.format({
+       pathname:"signup",
+       query: {
+          "email":email,
+        }
+     }));
+});
 ////////////////////////////////////
 //dynamic port : process.env.PORT defined by heroku
 app.listen(process.env.PORT || 3000, function(){
     console.log("Server is running on port 3000");
 });
-
-//apikey
-//594e723fbd4165a11a82a403db72d4d0-us20
-//34c56262470f1348bd512ce1503de4d5-us20
-
-//unique id
-//eedf70aa88
