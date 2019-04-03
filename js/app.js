6e3a32e73d97c978b44c196a05f3ab42adc7f0be
jaewonYoon <21300492@handong.edu>
2019-04-04 01:36:00 +0900

    add google authentication

--- js/app.js
+++ js/app.js
@@ -10,6 +10,13 @@ const _ = require("lodash");
 const app = express();
 const url = require('url');    
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt"); 
+const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate"); 
 //blog default writings
 const homeStartingContent = {
           title: "homestartingContent", 
@@ -31,9 +38,22 @@ app.use(express.static(path.join(__dirname,'../')));
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 
-///mongoose network connect///
+//setup session; initial configuration 
+//We set up sessions 
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false,
+}));
+//we initialize passport
+app.use(passport.initialize()); 
+//we use passport to manage our session 
+app.use( passport.session());
 
+///mongoose network connect///
 mongoose.connect("mongodb://localhost:27017/blogDB",{useNewUrlParser: true});
+mongoose.set("useCreateIndex", true);
+
 //mongoose.connect("mongodb+srv://admin-jaewon:test123@cluster0-ehw4p.mongodb.net/blogDB",{useNewUrlParser: true});
 
 //database variable// 
@@ -41,47 +61,186 @@ const postSchema = {
      title: String, 
      content:String,
 };
+//build userSchema used in signup
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String, 
+    googleId: String,
+    secret: [postSchema], 
+});
 
 const contentSchema = {
     name: String,
     title:[postSchema],
 }
+
 //build collection of data. 
 const Post = mongoose.model("post",postSchema);
 const List = mongoose.model("list", contentSchema);
 
+//이제 passport를 User모델에 사용할 수 있다. 
+//we set up schema to use passportLocalMongoose as a plugin 
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+//build collection name 'User' to use in userDB, 
+const User = new mongoose.model("User",userSchema);
+
+// finally we use passport localMongoose to use local login Strategy 
+// CHANGE: USE "createStrategy" INSTEAD OF "authenticate"
+passport.use(User.createStrategy());
+
+//we set up serializeUser and deserializeUser
+//passport for session use 
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+
+
+/*
+    Notice!
+    we should set the session before use serialize session and deserialize sesson 
+    Also, we have to initialize our passport before use strategies.
+*/
+
+
+
+//google Oauth 2.0 
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/home",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+  },
+  //google sends back accessToken allow us to use data related to user. 
+  //allow us to use user's data longer period 
+  //profile gets email, id etc. 
+  function(accessToken, refreshToken, profile, cb) {
+    console.log("show profile: ");
+    console.log(profile);
+    User.findOrCreate({ googleId:profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+
+
 
 ///routes/// 
 
-app.get("/",function(req,res){
+app.get("/home",function(req,res){
    console.log("home directory is underway");
    res.render("home"); 
 });
 
+//google authentication page
+app.get("/auth/google",
+  passport.authenticate("google", { scope:["profile"] })
+);
+
+//google authentication redirect
+app.get("/auth/google/home", 
+  //authenticate them locally
+  passport.authenticate('google', { failureRedirect: '/signup' }),
+  function(req, res) {
+    //once we authenticated, 
+//    if(req.isAuthenticated()){
+        res.redirect("/home");
+//    } else {
+//        res.redirect("/login");
+//    }
+//    // Successful authentication, redirect home.
+//    res.redirect('/secrets');
+  });
+//webcam routes
 app.get("/webcam",function(req,res){
     console.log("webcam directory is underway");
     res.render("home2");
 });
 
 app.get("/pricing", function(req,res){
-    res.sendFile(path.join(__dirname,"../pricing.html"));
+//    res.sendFile(path.join(__dirname,"../pricing.html"));
+    res.render("pricing");
 });
  
     //signup
 app.get("/signup", function(req,res){
-    if(req.url.query){ 
-      console.log(req.url.query.email);
-      res.render("signup",{email_req: req.url.query.email});
-    }
-    else res.render("signup",{email_req: ''}); //is null 
+//    if(req.url.query){ 
+//      console.log(req.url.query.email);      
+//    }
+    if(req.isAuthenticated()){
+        res.redirect("/home");
+    }else{
+    res.render("signup");
+    }//is null 
 //    res.sendFile(path.join(__dirname,"../signup.html"));
 });
 
+//post sign up 
 app.post("/signup",function(req,res){
-    let firstName =req.body.fName; 
-    let lastName = req.body.lName;
+//    let nickname =req.body.nickname; 
+    let passwordcheck = req.body.passwordcheck;
+    let email = req.body.username;
+    let password = req.body.password;
+   User.register({username:email},password,function(err,user){
+     if(err){
+         console.log(err);
+         res.redirect('/signup');
+     } else{
+         console.log("before authenticate");
+         passport.authenticate("local")(req,res,function(){
+             console.log("after authenticate");
+             res.redirect('/');
+         });
+     }
+   });
+});
+
+//route for logout page 
+app.get("/logout", function(req, res){
+  console.log(req.user);
+  req.logout();
+  res.redirect("/");
+});
+//get in login page
+app.get("/",function(req,res){
+  res.render("login");
+});
+//post in login page
+app.post("/login", function(req,res){
+    
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password,
+    });
+    //method comes from passport 
+    req.login(user,function(err){
+              if(err){
+                console.log(err);
+            }else{
+                passport.authenticate("local")(req,res,function(){
+                    res.redirect("/home");
+                });
+            }
+    });
+});
+//subscribe page
+app.get("/subscribe_mailchimp",function(req,res){
+    res.render("subscribe"); 
+});
+//post  subscribe 
+app.post("/subscribe_mailchimp",function(req,res){
     let email = req.body.email;
-    let image = req.body.image;
+    let image = req.body.image_url;
     
     let data = {
         members: [
@@ -89,8 +248,62 @@ app.post("/signup",function(req,res){
                 email_address: email,
                 status: "subscribed",
                 merge_fields: {
-                    FNAME: firstName,
-                    LNAME: lastName,
+                    FNAME: "somebody's FNAME",
+                    LNAME: "somebody's LNAME",
+                    IMAGE: "",
+                }
+            }
+        ]
+    };
+    //sign up 페이지에서 mailchimp 사용을 위한 코드 
+    let jsonData = JSON.stringify(data);
+    let options={
+        url:"https://us20.api.mailchimp.com/3.0/lists/eedf70aa88",
+        method: "POST",
+        headers: {
+            "Authorization" : "21300492@handong.edu "+ process.env.API_KEY,
+        },
+        body: jsonData
+    };
+    //sign up 페이지에서 성공과 실패를 구분하는 페이지 
+    request(options, function(error, response, body){
+        if(error){
+            res.sendFile(path.join(__dirname,"../failure.html"));
+        } else {
+            if(response.statusCode===200){
+    // res.sendFile로 응답해도 되나 ejs 사용을 위해 주석처리함                
+    // res.sendFile(path.join(__dirname,"../success.html"));
+                res.render("success");
+            }else {
+    // res.sendFile로 응답해도 되나 ejs 사용을 위해 주석처리함                
+    // res.sendFile(path.join(__dirname,"../failure.html"));
+                res.render("success");
+    // res.sendFile로 응답해도 되나 ejs 사용을 위해 주석처리함                
+    // res.sendFile(path.join(__dirname,"../failure.html"));         
+                res.render("failure");
+            }
+        }
+    });
+});
+//get stylerequest page
+app.get("/submit_mailchimp",function(req,res){
+        res.render("submit");
+});
+//using mailchimp submit page
+app.post("/submit_mailchimp",function(req,res){
+//    let firstName =req.body.fName; 
+//    let lastName = req.body.lName;
+    let email = req.body.email;
+    let image = req.body.image_url;
+    
+    let data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: "somebody's FNAME",
+                    LNAME: "somebody's LNAME",
                     IMAGE: image,
                 }
             }
@@ -129,7 +342,7 @@ app.post("/signup",function(req,res){
 
 //failure
 app.post("/failure", function(req,res){
-    res.redirect("/");
+    res.redirect("/home");
 });
 
 ////////routes for about 페이지 
@@ -195,7 +408,8 @@ app.get("/posts/:customListName",function(req,res){
 //routes for compose 페이지 
 app.get("/compose",function(req,res){
     res.render("compose");
-})
+});
+    
 app.post("/compose",function(req,res){
 //remove static variable 
 //  const post = {
@@ -243,7 +457,7 @@ app.post("/masthead",function(req,res){
     let email = req.body.email;
     console.log(email);
     res.redirect(url.format({
-       pathname:"signup",
+       pathname:"submit_mailchimp",
        query: {
           "email":email,
         }
